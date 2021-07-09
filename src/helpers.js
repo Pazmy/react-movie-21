800e4ce721ed7e5c17593e513a17299eecaad3ce
Pazmy <fadhillah17azmy01@gmail.com>
2021-07-09 09:27:13 +0700

    small changes

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,10 @@
-// Convert time to hours and minutes
+
 export const calcTime = (time) => {
   const hours = Math.floor(time / 60);
   const mins = time % 60;
   return `${hours}h ${mins}m`;
 };
-// Convert a number to money formatting
+
 export const convertMoney = (money) => {
   const formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -13,7 +13,7 @@ export const convertMoney = (money) => {
   });
   return formatter.format(money);
 };
-// Session Storage
+
 export const isPersistedState = (stateName) => {
   const sessionState = sessionStorage.getItem(stateName);
   return sessionState && JSON.parse(sessionState);
